568f01f9e8a446ff8cad4d911086a8af684b950d
Natan <natan.devback@gmail.com>
2024-04-01 19:08:33 -0300

    update of the code

--- src/services/createVideoRepository.ts
+++ src/services/createVideoRepository.ts
@@ -1,5 +1,4 @@
 
-import { Repository } from "typeorm";
 import { Database } from "../dataSource";
 import { Category} from "../entities/category";
 import { Video } from "../entities/Video";
@@ -9,16 +8,17 @@ type createVideoRequest={
     name :string,
     description:string,
     duration:number,
-    category_id:string, 
+    id:string ,
+    category_id:string
  
 }
 
 export class createVideoRepository{
-    async execute({name,description,duration,category_id,}:createVideoRequest):Promise<Error | Video>{
+    async execute({name,description,duration,category_id,id,}:createVideoRequest):Promise<Error | Video>{
       const repo= Database.getRepository(Video)
       const repoCategory = Database.getRepository(Category)
 
-      if(!(await repoCategory.findOneBy({}))){
+      if(!(await repoCategory.findOneBy({id}))){
         return new Error("Category does not exists")
       }
      const video = repo.create({name,description,duration,category_id})
